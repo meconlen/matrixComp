64e2c5241242199143c78df02e423dd1d6364f9b
Michael Conlen <mike@conlen.org>
2014-05-19 17:56:13 -0400

    updates

--- src/C/measure.c
+++ src/C/measure.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include <sys/time.h>
 #include <sys/resource.h>
@@ -13,35 +14,42 @@ int main(int argc, char *argv[], char *envp[])
 {
 	double 	*A, *B, *C;
 	double elapsed;
-	int 	i;
-	char 	c;
-
+	int 		i;
+	uint64_t	n;
+	char 		c;
 
 	struct rusage 	ruStart, ruEnd;
 
-	
+	while((c = getopt(argc, argv, "n:")) != -1)
+		switch(c) {
+			case 'n': 
+				n = atoll(optarg);
+				break;
+			default:
+				goto error0;
+		}
 
 
-	A = calloc(SIZE*SIZE, sizeof(double));
-	B = calloc(SIZE*SIZE, sizeof(double));
-	C = calloc(SIZE*SIZE, sizeof(double));
+	A = calloc(n*n, sizeof(double));
+	B = calloc(n*n, sizeof(double));
+	C = calloc(n*n, sizeof(double));
 
-	for(i=0; i<SIZE*SIZE; i++) {
+	for(i=0; i<n*n; i++) {
 		A[i] = drand48();
 		B[i] = drand48();
 	}
-	if(SIZE < 10) { 
+	if(n < 10) { 
 		printf("A = \n");
-		printMatrix(A, SIZE, SIZE);
+		printMatrix(A, n, n);
 		printf("\nB = \n");
-		printMatrix(B, SIZE, SIZE);
+		printMatrix(B, n, n);
 	}
 	getrusage(RUSAGE_SELF, &ruStart);
-	dMM(A, B, C, SIZE, SIZE, SIZE);
+	dMM(A, B, C, n, n, n);
 	getrusage(RUSAGE_SELF, &ruEnd);
 	if(SIZE < 10) {
 		printf("\nC = \n");
-		printMatrix(C, SIZE, SIZE);
+		printMatrix(C, n, n);
 	}
 	elapsed =  ruEnd.ru_utime.tv_sec;
 	elapsed += (double)ruEnd.ru_utime.tv_usec / 1000000;
@@ -49,8 +57,12 @@ int main(int argc, char *argv[], char *envp[])
 	elapsed -= (double)ruStart.ru_utime.tv_usec / 1000000;
 
 	printf("time = %f\n", elapsed);
-	printf("Operations = %d\n", 2*SIZE*SIZE*SIZE);
-	printf("GLFOPS = %f\n", (2.0*SIZE*SIZE*SIZE)/1000000000 / elapsed);
+	printf("Operations = %d\n", 2*n*n*n);
+	printf("GLFOPS = %f\n", (2.0*n*n*n)/1000000000 / elapsed);
 
 	return(0);
+error0:
+	fprintf(stderr, "Usage: %s -n N\n", argv[0]);
+	fprintf(stderr, "N = matrix size\n");
+	exit(-1);
 }
