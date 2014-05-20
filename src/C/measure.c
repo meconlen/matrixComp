02b530b70bca630dfe72c776824ba5028423fd41
Michael Conlen <mike@conlen.org>
2014-05-20 19:29:09 -0400

    fixup time.h

--- src/C/measure.c
+++ src/C/measure.c
@@ -2,7 +2,18 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#include <sys/time.h>
+#ifdef TIME_WITH_SYS_TIME
+	# include <sys/time.h>
+	# include <time.h>
+#else
+	#ifdef HAVE_SYS_TIME_H
+		#include <sys/time.h>
+	#else
+		#include <time.h>
+	#endif
+#endif
+
+
 #include <sys/resource.h>
 
 
