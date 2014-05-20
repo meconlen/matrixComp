c10e15aa9f1d27de800669e1bc0158521584c5e1
Michael Conlen <mike@conlen.org>
2014-05-20 18:31:52 -0400

    fixed up some bugs in counting

--- src/C/measure.c
+++ src/C/measure.c
@@ -12,9 +12,9 @@
 int main(int argc, char *argv[], char *envp[])
 {
 	double 			*A, *B, *C;
-	double 			elapsed;
+	double 			elapsed = 0, totalElapsed = 0;
 	double 			gflops = 0;
-	int 			i, j;
+	int 			i, j, k;
 	unsigned int	start = 1, end = 1, skip = 1, count = 1;
 	unsigned int	size = 1, algorithm = 1, debug = 0;
 	char 			c;
@@ -47,13 +47,16 @@ int main(int argc, char *argv[], char *envp[])
 	fprintf(stderr, "start = %d, end = %d, skip = %d, count = %d\n",
 		start, end, skip, count);
 	for(size=start; size<=end; size+=skip) {
+		if(debug > 0) fprintf(stderr, "Starting size = %d\n", size);
+		totalElapsed = 0;
 		for(j=0; j<count; j++) {
+			if(debug > 0) fprintf(stderr, "Computing matrix count = %d\n", j);
 			A = calloc(size * size, sizeof(double));
 			B = calloc(size * size, sizeof(double));
 			C = calloc(size * size, sizeof(double));
-			for(j=0; j<size * size; j++) {
-				A[j] = drand48();
-				B[j] = drand48();
+			for(k=0; k<size * size; k++) {
+				A[k] = drand48();
+				B[k] = drand48();
 			}
 			getrusage(RUSAGE_SELF, &ruStart);
 			switch(algorithm) {
@@ -66,23 +69,25 @@ int main(int argc, char *argv[], char *envp[])
 			}
 
 			getrusage(RUSAGE_SELF, &ruEnd);
+
+			elapsed =  ruEnd.ru_utime.tv_sec;
+			elapsed += (double)ruEnd.ru_utime.tv_usec / 10e6;
+			elapsed -= ruStart.ru_utime.tv_sec;
+			elapsed -= (double)ruStart.ru_utime.tv_usec / 10e6;
+			totalElapsed += elapsed;
 			if(debug > 0) {
-				fprintf(stderr, "size = %d, alg = %d, start = %f, end = %f\n",
+				fprintf(stderr, "size = %d, alg = %d, start = %f, end = %f, elapsed = %f, Gops = %f\n",
 					size, algorithm, 
-					ruStart.ru_utime.tv_sec + (double)(ruStart.ru_utime.tv_usec)/1000000,
-					ruEnd.ru_utime.tv_sec + (double)(ruEnd.ru_utime.tv_usec)/1000000
+					ruStart.ru_utime.tv_sec + (double)(ruStart.ru_utime.tv_usec)/10e6,
+					ruEnd.ru_utime.tv_sec + (double)(ruEnd.ru_utime.tv_usec)/10e6,
+					elapsed, (double)2.0*size*size*size/10e9
 				);
 			}
-			elapsed =  ruEnd.ru_utime.tv_sec;
-			elapsed += (double)ruEnd.ru_utime.tv_usec / 1000000;
-			elapsed -= ruStart.ru_utime.tv_sec;
-			elapsed -= (double)ruStart.ru_utime.tv_usec / 1000000;
-			gflops += ((2.0*size*size*size)/1000000000)/elapsed;
 			free(A);
 			free(B);
 			free(C);		
 		}
-		gflops /= count;
+		gflops = (((double)2.0*size*size*size*count)/10e9)/totalElapsed;
 		printf("%u %u %f\n", size, count, gflops);
 	}
 	return(0);
